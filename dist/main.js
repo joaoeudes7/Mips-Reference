9b88dda36fd325bf1b6159195b97be41f37e3cc8
João Eudes Lima <joaoeudes7@gmail.com>
2019-09-02 00:06:48 -0300

    Removed folder dist

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const binToAssembly_1 = require("./src/binToAssembly");
-const BIN = "0x21490064";
-const FORMAT = binToAssembly_1.Format.hex;
-const BITS_DEFAULT = 32;
-binToAssembly_1.binToAssembly(BIN, FORMAT, BITS_DEFAULT);
